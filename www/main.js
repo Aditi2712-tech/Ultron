ce91d224c35c9829a4ed394b7004003377f2b0ea
Aditi2712-tech <abbg149209@gmail.com>
2026-06-21 19:41:34 +0530

    hood design

---
+++ www/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+    $('.text').textillate({
+        loop: true,
+        in: {
+            effect: 'bounceIn'
+        },
+        out: {
+            effect: 'bounceOut'
+        }
+    });
+});

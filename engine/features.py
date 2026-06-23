130168862c8e8226ac57c19a405ac9617bf9eff8
Aditi2712-tech <abbg149209@gmail.com>
2026-06-24 01:43:10 +0530

    siriWave and starting sound

---
+++ engine/features.py
@@ -0,0 +1,7 @@
+from playsound import playsound
+
+def playStartupSound():
+    playsound("www\\assets\\audio\\soundtrack1_segment_0_to_4.mp3")
+
+def playRemainingSound():
+    playsound("www\\assets\\audio\\soundtrack1_segment_4_to_6.mp3")
--- www/main.js
+++ www/main.js
@@ -9,4 +9,28 @@ $(document).ready(function () {
             effect: 'bounceOut'
         }
     });
+
+    //siri config
+    var siriWave = new SiriWave({
+    container: document.getElementById("siri-container"),
+    width: 800,
+    height: 180,
+    style: "ios9",
+    amplitude: "1",
+    speed: "0.30",
+    autostart: true,
+
+    });
+
+    $('.siri-message').textillate({
+        loop: true,
+        in: {
+            effect: 'rotateIn',
+            sequence: true
+        },
+        out: {
+            effect: 'rotateOut',
+            reverse: true
+        }
+    });
 });

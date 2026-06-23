8d9d6b96c6239c67d10be85a4b8181b1e93c3522
Aditi2712-tech <abbg149209@gmail.com>
2026-06-24 01:43:10 +0530

    siriWave and starting sound

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import os
+import threading #for playing audios in different threads
 import eel #connecting backend to frontend using this lib
+
+from engine.features import *
+
 eel.init("www") #telling eel the dir containing frontend
 
+# play first half
+playStartupSound()
+
 os.system("start msedge.exe --app=\"http://localhost:8000/index.html\"")
+
+# play second half in background
+threading.Thread(target=playRemainingSound, daemon=True).start()
 
 eel.start("index.html", mode=None, host='localhost', block=True)

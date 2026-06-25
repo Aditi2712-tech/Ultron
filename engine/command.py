3463a994c56e9899098cd471ca7e842ef04b822e
Aditi2712-tech <abbg149209@gmail.com>
2026-06-26 01:17:59 +0530

    opening apps and yt

--- engine/command.py
+++ engine/command.py
@@ -1,6 +1,7 @@
 import pyttsx3
 import speech_recognition as sr
 import eel
+import time
 
 
 def speak(text):
@@ -8,18 +9,20 @@ def speak(text):
     voices = engine.getProperty('voices')      # getting details of current voice
     engine.setProperty('voice', voices[0].id)
     engine.setProperty('rate', 175)     # setting up new voice rate
+    eel.DisplayMessage(text)
     # print(voices)
     engine.say(text)
     engine.runAndWait()
 
 
-@eel.expose
+
 def takecommand():
 
     r = sr.Recognizer()
 
     with sr.Microphone() as source:
         print("listening.....")
+        # time.sleep(1)
         eel.DisplayMessage("Listening.....")
         r.pause_threshold
         r.adjust_for_ambient_noise(source)
@@ -29,13 +32,12 @@ def takecommand():
     try:
         print("Recognizing...")
         eel.DisplayMessage("Recognizing.....")
-
-
         query = r.recognize_google(audio, language="en-in")
         print(f"User: {query}")
         eel.DisplayMessage(query)
-        speak(query)
-        eel.ShowHood()
+        time.sleep(2)
+        # speak(query)
+
     except Exception as e:
         return "Couldn't Hear You....."
 
@@ -44,5 +46,21 @@ def takecommand():
 
 # text = takecommand()
 # speak(text)
+ 
 
+@eel.expose
+def allCommand():
 
+    query = takecommand()
+    print(query)
+
+    if "open" in query:
+        from engine.features import openCommand
+        openCommand(query)
+    elif "on youtube":
+        from engine.features import PlayYoutube
+        PlayYoutube(query)
+    else:
+        print("Not run")
+
+    eel.ShowHood()
